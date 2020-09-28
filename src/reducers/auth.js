ed56d4885b5cfc40e67a63390ef3d3cbc91eae7a
Restyled.io <commits@restyled.io>
2020-09-28 14:53:57 +0000

    Restyled by prettier

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -6,49 +6,61 @@ import {
   LOGOUT_REQUEST,
   LOGOUT_SUCCESS,
   VERIFY_REQUEST,
-  VERIFY_SUCCESS
+  VERIFY_SUCCESS,
 } from "../actions/";
 
-export default (state = {
-  isLoggingIn : false,
-  isLoading : false,
-  isLoggingOut : false,
-  isVerifying : false,
-  loginError : false,
-  logoutError : false,
-  isAuthenticated : false,
-  user : {}
-},
-                action) => {
+export default (
+  state = {
+    isLoggingIn: false,
+    isLoading: false,
+    isLoggingOut: false,
+    isVerifying: false,
+    loginError: false,
+    logoutError: false,
+    isAuthenticated: false,
+    user: {},
+  },
+  action
+) => {
   switch (action.type) {
-  case LOGIN_REQUEST:
-    return {...state, isLoggingIn : true, isLoading : true, loginError : false};
-  case LOGIN_SUCCESS:
-    return {
-      ...state,
-      isLoggingIn : false,
-      isLoading : false,
-      isAuthenticated : true,
-      user : action.user
-    };
-  case LOGIN_FAILURE:
-    return {
-      ...state,
-      isLoggingIn : false,
-      isAuthenticated : false,
-      loginError : true
-    };
-  case LOGOUT_REQUEST:
-    return {...state, isLoggingOut : true, logoutError : false};
-  case LOGOUT_SUCCESS:
-    return {...state, isLoggingOut : false, isAuthenticated : false, user : {}};
-  case LOGOUT_FAILURE:
-    return {...state, isLoggingOut : false, logoutError : true};
-  case VERIFY_REQUEST:
-    return {...state, isVerifying : true, verifyingError : false};
-  case VERIFY_SUCCESS:
-    return {...state, isVerifying : false};
-  default:
-    return state;
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        isLoggingIn: true,
+        isLoading: true,
+        loginError: false,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoading: false,
+        isAuthenticated: true,
+        user: action.user,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isLoggingIn: false,
+        isAuthenticated: false,
+        loginError: true,
+      };
+    case LOGOUT_REQUEST:
+      return { ...state, isLoggingOut: true, logoutError: false };
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        isLoggingOut: false,
+        isAuthenticated: false,
+        user: {},
+      };
+    case LOGOUT_FAILURE:
+      return { ...state, isLoggingOut: false, logoutError: true };
+    case VERIFY_REQUEST:
+      return { ...state, isVerifying: true, verifyingError: false };
+    case VERIFY_SUCCESS:
+      return { ...state, isVerifying: false };
+    default:
+      return state;
   }
 };
