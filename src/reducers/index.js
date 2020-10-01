8e1e8a6997f6234859d42f9c595abcdf690e7e61
Asidipta <asidiptachaudhuri@gmail.com>
2020-10-01 22:35:55 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,8 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
+import links from './links';
+import filter from './filter';
+
 
 export default combineReducers({ auth });
+export default combineReducers({ auth, links, filter });
