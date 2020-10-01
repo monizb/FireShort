d264ceb133160e0d100831ac244c9272dc179b22
Asidipta <asidiptachaudhuri@gmail.com>
2020-10-01 22:36:28 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,4 @@ import auth from "./auth";
 import links from './links';
 import filter from './filter';
 
-
-export default combineReducers({ auth });
 export default combineReducers({ auth, links, filter });
