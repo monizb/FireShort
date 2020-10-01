5459c6c5e2f06547869ca74ff0ae25968ba88799
Asidipta <asidiptachaudhuri@gmail.com>
2020-10-01 22:34:12 +0530

    Update MainToolBar.js

--- src/components/MainToolBar.js
+++ src/components/MainToolBar.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button, ButtonGroup, Card, Container } from '@material-ui/core';
+import { Button, ButtonGroup, Card, Container, TextField } from '@material-ui/core';
 import { ViewModule as ViewModuleIcon, ViewList as ViewListIcon, Loop } from '@material-ui/icons';
+
+import { setTextFilter } from '../actions';
 
 const useStyles = makeStyles((theme) => ({
     toolBarRoot: {
@@ -15,10 +18,18 @@ const useStyles = makeStyles((theme) => ({
 
 export default function MainToolBar(props) {
     const classes = useStyles();
+    
+    const filter = useSelector((state) => state.filter)
+    const dispatch = useDispatch();
+
+    const changeHandler = (value) => {
+        dispatch(setTextFilter(value));
+    }
 
     return (
         <Container maxWidth="md">
-            <Card className={classes.toolBarRoot}>
+            <Card className={classes.toolBarRoot} style={{ display: "grid", gridTemplateColumns: "3fr 1fr" }}>
+                <TextField variant="outlined" color="default" value={filter.text} placeholder="Search links" onChange={(e) => changeHandler(e.target.value)} style={{ boxShadow: "unset !important", justifyContent: "flex-start", display: "flex" }} />
                 <ButtonGroup variant="outlined" color="default" style={{ boxShadow: "unset !important", justifyContent: "flex-end", display: "flex" }}>
                     <Button onClick={props.refresh}>
                         <Loop />
