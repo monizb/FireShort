66f01af9b5475fadedd9287641f76058d2852559
Abhinav Kumar <sahabhinav36@gmail.com>
2020-10-03 03:57:49 +0530

    added security rules, admin panel, improved db structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,8 +55,21 @@ class Home extends Component {
                 let ipv4 = "";
                 (async () => {
                   ipv4 = await publicIp.v4();
-                  docref.update({
+                  //update the hits count
+                  const hitref = db.collection('hits').doc(docid);
+                  hitref
+                  .update({
                     hits: firebase.firestore.FieldValue.increment(1),
+                  })
+                  .catch(error=>{
+                    //hit document is not created yet.
+                    if(error.code)
+                    hitref
+                    .set({
+                      hits: 1,
+                      curl: docid,
+                      author: data.author
+                    });
                   });
                   docref
                     .collection("tracking")
