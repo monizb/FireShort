31478b7b027ac9788b53e030bb7c820748ebb0cc
Restyled.io <commits@restyled.io>
2020-10-02 13:49:51 +0000

    Restyled by clang-format

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,96 +1,93 @@
-import React, { Component } from 'react';
-import { db } from '../firebase/firebase';
-import firebase from 'firebase';
-import classes from './components.module.css';
-import publicIp from 'public-ip';
 import '../config';
+
+import firebase from 'firebase';
+import publicIp from 'public-ip';
+import React, {Component} from 'react';
+
+import {db} from '../firebase/firebase';
+
+import classes from './components.module.css';
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
-      loc: window.location.pathname,
-      newloc: '',
-      loading: true,
-      isLocked: true,
-      realPassword: '',
-      password: '',
+      loc : window.location.pathname,
+      newloc : '',
+      loading : true,
+      isLocked : true,
+      realPassword : '',
+      password : '',
     };
     document.body.style =
-      'background: #f1f1f1; margin: 0; padding: 0; min-height: 100vh; display: flex;justify-content: center;align-items: center;';
-    
+        'background: #f1f1f1; margin: 0; padding: 0; min-height: 100vh; display: flex;justify-content: center;align-items: center;';
   }
 
-  componentDidMount() {
-    this.startFunc(true);
-  }
+  componentDidMount() { this.startFunc(true); }
 
   startFunc = (isLocked) => {
-    this.setState({ loading: true });
+    this.setState({loading : true});
     if (this.state.loc === '/') {
-      this.setState({ newloc: global.config.mainsite });
+      this.setState({newloc : global.config.mainsite});
       window.location = global.config.mainsite;
     } else {
       var docid = this.state.loc.substring(1);
       var docref = db.collection('shorturls').doc(docid);
-      docref
-        .get()
-        .then((doc) => {
-          var data = doc.data();
-          this.setState({ loading: false, realPassword: data.password });
-          if (!doc.exists) {
-            window.location.pathname = '/login';
-          } else {
-            if (!data.locked || !isLocked) {
-              this.setState({ isLocked: false });
-              if (data.track === false) {
-                docref.update({ hits: firebase.firestore.FieldValue.increment(1) });
-                this.setState({ newloc: data.lurl });
-                window.location = data.lurl;
-              } else {
-                let ipv4 = '';
-                (async () => {
-                  ipv4 = await publicIp.v4();
-                  docref.update({ hits: firebase.firestore.FieldValue.increment(1) });
-                  docref
-                    .collection('tracking')
-                    .add({
-                      ipv4: ipv4,
-                      timestamp: new Date().toLocaleString(),
-                      useragent: navigator.userAgent,
-                    })
-                    .then(() => {
-                      this.setState({ newloc: data.lurl });
-                      window.location = data.lurl;
-                    });
-                })();
-              }
+      docref.get()
+          .then((doc) => {
+            var data = doc.data();
+            this.setState({loading : false, realPassword : data.password});
+            if (!doc.exists) {
+              window.location.pathname = '/login';
             } else {
-              this.setState({ isLocked: true });
+              if (!data.locked || !isLocked) {
+                this.setState({isLocked : false});
+                if (data.track === false) {
+                  docref.update(
+                      {hits : firebase.firestore.FieldValue.increment(1)});
+                  this.setState({newloc : data.lurl});
+                  window.location = data.lurl;
+                } else {
+                  let ipv4 = '';
+                  (async () => {
+                    ipv4 = await publicIp.v4();
+                    docref.update(
+                        {hits : firebase.firestore.FieldValue.increment(1)});
+                    docref.collection('tracking')
+                        .add({
+                          ipv4 : ipv4,
+                          timestamp : new Date().toLocaleString(),
+                          useragent : navigator.userAgent,
+                        })
+                        .then(() => {
+                          this.setState({newloc : data.lurl});
+                          window.location = data.lurl;
+                        });
+                  })();
+                }
+              } else {
+                this.setState({isLocked : true});
+              }
             }
-          }
-        })
-        .catch((err) => {
-          console.log('Error getting document', err);
-        });
+          })
+          .catch((err) => { console.log('Error getting document', err); });
     }
   };
 
-  checkpassword = () => {
-    const { realPassword } = this.state;
-    if (realPassword !== '' && this.state.password === realPassword) {
-      this.setState({ isLocked: false }, () => this.startFunc(false));
-    } else {
-      alert("Invalid Link Password")
-    }
-  }
+  checkpassword =
+      () => {
+        const {realPassword} = this.state;
+        if (realPassword !== '' && this.state.password === realPassword) {
+          this.setState({isLocked : false}, () => this.startFunc(false));
+        } else {
+          alert("Invalid Link Password")
+        }
+      }
 
-  onPasswordChange = (e) => {
-    this.setState({ password: e.target.value });
-  };
+  onPasswordChange = (e) => { this.setState({password : e.target.value}); };
 
   render() {
-    const { loading, password, isLocked } = this.state;
+    const {loading, password, isLocked} = this.state;
     return (
       <div>
         {loading ? (
@@ -115,7 +112,10 @@ class Home extends Component {
                 style={{ padding: "15px", fontSize: "15px", borderRadius: "2px", width: "100%" }}
                 onChange={(e) => this.onPasswordChange(e)}
               />
-              <button onClick={(e) => this.checkpassword()} style={{ padding: "12px", color: "white", backgroundColor: "black", fontSize: "15px", border: "none", marginRight: "15px", borderRadius: "5px", cursor: "pointer", marginLeft: "10px" }}>Go</button>
+              <button onClick={(e) => this.checkpassword()} style={{
+      padding: "12px", color: "white", backgroundColor: "black",
+          fontSize: "15px", border: "none", marginRight: "15px",
+          borderRadius: "5px", cursor: "pointer", marginLeft: "10px" }}>Go</button>
             </div>
           </div>
         ) : null}
