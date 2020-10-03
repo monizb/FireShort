77cfa29304b01b84441c01b7c556bf89dd175f42
Niyas Muhammed <niyasmhdth@gmail.com>
2020-10-03 11:37:40 +0530

    Fixed Logout Handler @admin dashboard

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import MainToolBar from './MainToolBar.js';
-import CardUrls from './CardUrls.js';
-import ListUrls from './ListUrls.js';
-import HitsDialog from './HitsDialog.js';
-import UrlsDialog from './UrlsDialog.js';
-import Footer from './Footer.js';
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { nanoid } from 'nanoid';
-import { connect } from 'react-redux';
-import { confirmAlert } from 'react-confirm-alert'; // Import
-import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+import React, { Component } from "react";
+import MainToolBar from "./MainToolBar.js";
+import CardUrls from "./CardUrls.js";
+import ListUrls from "./ListUrls.js";
+import HitsDialog from "./HitsDialog.js";
+import UrlsDialog from "./UrlsDialog.js";
+import Footer from "./Footer.js";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { nanoid } from "nanoid";
+import { connect } from "react-redux";
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
 import { logoutUser, addLink, setLinks } from "../actions";
-import { getFilteredLinks } from '../selectors';
-import { myFirebase, db } from '../firebase/firebase';
-import './components.module.css';
+import { getFilteredLinks } from "../selectors";
+import { myFirebase, db } from "../firebase/firebase";
+import "./components.module.css";
 
 import {
   AppBar,
@@ -26,14 +26,14 @@ import {
   Snackbar,
   Toolbar,
   Typography,
-} from '@material-ui/core';
+} from "@material-ui/core";
 
-import { withStyles } from '@material-ui/core/styles';
-import AddIcon from '@material-ui/icons/Add';
-import MuiAlert from '@material-ui/lab/Alert';
+import { withStyles } from "@material-ui/core/styles";
+import AddIcon from "@material-ui/icons/Add";
+import MuiAlert from "@material-ui/lab/Alert";
 
 function Alert(props) {
-  return <MuiAlert elevation={6} variant='filled' {...props} />;
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 const styles = (theme) => ({
@@ -41,29 +41,29 @@ const styles = (theme) => ({
     flexGrow: 1,
   },
   passInput: {
-    padfing: 15
+    padfing: 15,
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
-    fontFamily: 'Pacifico, cursive',
-    userSelect: 'none',
-    color: 'white',
+    fontFamily: "Pacifico, cursive",
+    userSelect: "none",
+    color: "white",
   },
   fab: {
-    position: 'fixed',
+    position: "fixed",
     bottom: theme.spacing(2),
-    backgroundColor: '#212121',
+    backgroundColor: "#212121",
     right: theme.spacing(2),
   },
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
+    color: "#fff",
   },
   appbar: {
-    backgroundColor: '#212121',
+    backgroundColor: "#212121",
   },
 });
 
@@ -77,14 +77,14 @@ class Admin extends Component {
       hits: [],
       formopen: false,
       hitsopen: false,
-      lurl: '',
-      curl: '',
+      lurl: "",
+      curl: "",
       track: true,
       successToast: false,
-      viewMode: 'module',
+      viewMode: "module",
       backdrop: false,
       inputBackdrop: false,
-      newPsw: '',
+      newPsw: "",
       currUrl: null,
     };
     this.handleLurlChange = this.handleLurlChange.bind(this);
@@ -107,7 +107,7 @@ class Admin extends Component {
 
   createLink = (curl, data) => {
     const self = this;
-    db.collection('shorturls')
+    db.collection("shorturls")
       .doc(curl)
       .set(data)
       .then(function () {
@@ -120,7 +120,7 @@ class Admin extends Component {
     let curl = this.state.curl;
     let track = this.state.track;
     const self = this;
-    if (curl === '') {
+    if (curl === "") {
       curl = nanoid(8);
     }
     let data = {
@@ -129,28 +129,28 @@ class Admin extends Component {
       track: track,
       hits: 0,
       locked: false,
-      password: '',
+      password: "",
     };
-    db.collection('shorturls')
+    db.collection("shorturls")
       .doc(curl)
       .get()
       .then(function (docSnapshot) {
         if (docSnapshot.exists) {
           self.handleClose();
           confirmAlert({
-            title: 'Custom URL overwrite confirm',
+            title: "Custom URL overwrite confirm",
             message:
-              'The Custom URL you entered is alread associated with some other link, clicking ok will overwrite that link to the new one. Continue?',
+              "The Custom URL you entered is alread associated with some other link, clicking ok will overwrite that link to the new one. Continue?",
             buttons: [
               {
-                label: 'Yes',
+                label: "Yes",
                 onClick: () => {
                   self.createLink(curl, data);
                   self.updateUrls();
                 },
               },
               {
-                label: 'No',
+                label: "No",
               },
             ],
           });
@@ -168,11 +168,11 @@ class Admin extends Component {
     const url = shortUrls[shortUrls.findIndex((url) => url.id === curl)].data;
     this.setState({ currUrl: url });
     if (url.locked) {
-      db.collection('shorturls')
+      db.collection("shorturls")
         .doc(curl)
         .update({
           locked: false,
-          password: '',
+          password: "",
         })
         .then(() => this.updateUrls());
     } else {
@@ -184,35 +184,39 @@ class Admin extends Component {
     const self = this;
 
     confirmAlert({
-      title: 'Confirm Deletion',
-      message: 'Are you sure you want to delete this URL?',
+      title: "Confirm Deletion",
+      message: "Are you sure you want to delete this URL?",
       buttons: [
         {
-          label: 'Delete',
+          label: "Delete",
           onClick: () => {
-            db.collection('shorturls').doc(curl).delete().then(function () {
-              self.updateUrls();
-            });
-          }
+            db.collection("shorturls")
+              .doc(curl)
+              .delete()
+              .then(function () {
+                self.updateUrls();
+              });
+          },
         },
         {
-          label: 'Back',
-          onClick: () => { return; }
-        }
-      ]
+          label: "Back",
+          onClick: () => {
+            return;
+          },
+        },
+      ],
     });
-  }
-
+  };
 
   handleEditShortUrl = (curl) => {
     this.setState({ backdrop: true });
     const self = this;
-    var docref = db.collection('shorturls').doc(curl);
+    var docref = db.collection("shorturls").doc(curl);
     docref
       .get()
       .then((doc) => {
         if (!doc.exists) {
-          console.log('No such document!');
+          console.log("No such document!");
         } else {
           var data = doc.data();
 
@@ -224,14 +228,14 @@ class Admin extends Component {
         }
       })
       .catch((err) => {
-        console.log('Error getting document', err);
+        console.log("Error getting document", err);
       });
   };
 
   handleClickOpen = () => {
     this.setState({ formopen: true });
-    this.setState({ lurl: '' });
-    this.setState({ curl: '' });
+    this.setState({ lurl: "" });
+    this.setState({ curl: "" });
   };
 
   handleClose = () => {
@@ -239,7 +243,7 @@ class Admin extends Component {
   };
 
   handleToastClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
@@ -252,20 +256,23 @@ class Admin extends Component {
     self.setState({ loading: true });
     self.setState({ shortUrls: [] });
 
-    db.collection('shorturls')
+    db.collection("shorturls")
       .get()
       .then((snapshot) => {
         snapshot.forEach((doc) => {
           self.setState({
-            shortUrls: [...self.state.shortUrls, { id: doc.id, data: doc.data() }],
+            shortUrls: [
+              ...self.state.shortUrls,
+              { id: doc.id, data: doc.data() },
+            ],
           });
         });
-        self.props.setLink(self.state.shortUrls)
+        self.props.setLink(self.state.shortUrls);
         self.setState({ loading: false });
         this.setState({ backdrop: false });
       })
       .catch((err) => {
-        console.log('Error getting documents', err);
+        console.log("Error getting documents", err);
         self.setState({ loading: false });
       });
   };
@@ -275,27 +282,29 @@ class Admin extends Component {
     this.setState({ backdrop: true });
     self.setState({ loading: true });
     self.setState({ hits: [] });
-    db.collection('shorturls')
+    db.collection("shorturls")
       .doc(id)
-      .collection('tracking')
+      .collection("tracking")
       .get()
       .then((snapshot) => {
         snapshot.forEach((hit) => {
-          self.setState({ hits: [...self.state.hits, { id: hit.id, data: hit.data() }] });
+          self.setState({
+            hits: [...self.state.hits, { id: hit.id, data: hit.data() }],
+          });
         });
         self.setState({ loading: false });
         this.setState({ backdrop: false });
         self.setState({ hitsopen: true });
       })
       .catch((err) => {
-        console.log('Error getting documents', err);
+        console.log("Error getting documents", err);
         self.setState({ loading: false });
       });
   };
 
   updateViewMode = (mode) => {
     this.setState({ viewMode: mode });
-    db.collection('settings').doc('viewMode').set({ value: mode });
+    db.collection("settings").doc("viewMode").set({ value: mode });
   };
 
   componentDidMount() {
@@ -304,19 +313,19 @@ class Admin extends Component {
       if (user) {
         self.setState({ user });
         self.updateUrls();
-        var viewModeRef = db.collection('settings').doc('viewMode');
+        var viewModeRef = db.collection("settings").doc("viewMode");
         viewModeRef
           .get()
           .then((doc) => {
             if (!doc.exists) {
-              console.log('No viewMode set!');
+              console.log("No viewMode set!");
             } else {
               var data = doc.data();
               self.setState({ viewMode: data.value });
             }
           })
           .catch((err) => {
-            console.log('Error getting viewMode', err);
+            console.log("Error getting viewMode", err);
           });
       } else {
         self.setState({ user: null });
@@ -325,14 +334,13 @@ class Admin extends Component {
   }
 
   handleLogout = () => {
-    const { dispatch } = this.props;
-    dispatch(logoutUser());
+    this.props.logout();
   };
 
   onPswSave = (e) => {
     const curl = this.state.currUrl.curl;
-    if (this.state.newPsw !== '') {
-      db.collection('shorturls')
+    if (this.state.newPsw !== "") {
+      db.collection("shorturls")
         .doc(curl)
         .update({
           locked: true,
@@ -343,7 +351,7 @@ class Admin extends Component {
           this.updateUrls();
         });
     } else {
-      alert('Password cannot be empty.');
+      alert("Password cannot be empty.");
     }
   };
 
@@ -356,12 +364,12 @@ class Admin extends Component {
         {inputBackdrop ? (
           <div
             style={{
-              position: 'fixed',
-              width: '100vw',
-              height: '100vh',
-              background: 'rgb(0,0,0,.5)',
-              display: 'grid',
-              placeItems: 'center',
+              position: "fixed",
+              width: "100vw",
+              height: "100vh",
+              background: "rgb(0,0,0,.5)",
+              display: "grid",
+              placeItems: "center",
               zIndex: 10,
             }}
           >
@@ -369,14 +377,47 @@ class Admin extends Component {
               <h3>Protect Link With Password</h3>
               <div style={{ display: "block", padding: "20px" }}>
                 <input
-                  placeholder='Enter Password...'
+                  placeholder="Enter Password..."
                   value={newPsw}
-                  style={{ padding: "15px", fontSize: "15px", borderRadius: "2px", width: "100%" }}
+                  style={{
+                    padding: "15px",
+                    fontSize: "15px",
+                    borderRadius: "2px",
+                    width: "100%",
+                  }}
                   onChange={(e) => this.setState({ newPsw: e.target.value })}
                 />
                 <div style={{ marginTop: "25px" }}>
-                  <button onClick={(e) => this.onPswSave(e)} style={{ padding: "12px", color: "white", backgroundColor: "black", fontSize: "15px", border: "none", marginRight: "15px", borderRadius: "5px", cursor: "pointer" }}>Save</button>
-                  <button onClick={(e) => this.setState({ inputBackdrop: false })} style={{ padding: "12px", color: "white", backgroundColor: "red", fontSize: "15px", border: "none", marginRight: "15px", borderRadius: "5px", cursor: "pointer" }}>Cancel</button>
+                  <button
+                    onClick={(e) => this.onPswSave(e)}
+                    style={{
+                      padding: "12px",
+                      color: "white",
+                      backgroundColor: "black",
+                      fontSize: "15px",
+                      border: "none",
+                      marginRight: "15px",
+                      borderRadius: "5px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Save
+                  </button>
+                  <button
+                    onClick={(e) => this.setState({ inputBackdrop: false })}
+                    style={{
+                      padding: "12px",
+                      color: "white",
+                      backgroundColor: "red",
+                      fontSize: "15px",
+                      border: "none",
+                      marginRight: "15px",
+                      borderRadius: "5px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Cancel
+                  </button>
                 </div>
               </div>
             </div>
@@ -384,18 +425,18 @@ class Admin extends Component {
         ) : null}
         <CssBaseline />
         <div className={classes.root}>
-          <AppBar position='fixed' className={classes.appbar}>
+          <AppBar position="fixed" className={classes.appbar}>
             <Toolbar>
-              <Typography variant='h6' className={classes.title}>
+              <Typography variant="h6" className={classes.title}>
                 FireShort
               </Typography>
-              <Button color='inherit' onClick={this.handleLogout}>
+              <Button color="inherit" onClick={this.handleLogout}>
                 Logout
               </Button>
             </Toolbar>
           </AppBar>
         </div>
-        {this.state.loading && <LinearProgress color='secondary' />}
+        {this.state.loading && <LinearProgress color="secondary" />}
         <main>
           <MainToolBar
             state={this.state}
@@ -404,44 +445,44 @@ class Admin extends Component {
           />
           {this.state.shortUrls.length > 0 ? (
             <>
-              {this.state.viewMode === 'module' ? (
+              {this.state.viewMode === "module" ? (
                 <CardUrls
                   shortUrls={this.props.links}
                   handleEditShortUrl={this.handleEditShortUrl}
                   handleDeleteShortUrl={this.handleDeleteShortUrl}
                   openHits={this.getHits}
-                // updateHits={this.updateUrls}
+                  // updateHits={this.updateUrls}
                 />
               ) : (
-                  <ListUrls
-                    shortUrls={this.state.shortUrls}
-                    handleEditShortUrl={this.handleEditShortUrl}
-                    handleDeleteShortUrl={this.handleDeleteShortUrl}
-                    toggleSecurity={this.toggleSecurity}
-                  />
-                )}
+                <ListUrls
+                  shortUrls={this.state.shortUrls}
+                  handleEditShortUrl={this.handleEditShortUrl}
+                  handleDeleteShortUrl={this.handleDeleteShortUrl}
+                  toggleSecurity={this.toggleSecurity}
+                />
+              )}
             </>
           ) : (
-              <div className={classes.heroContent}>
-                <Container maxWidth='sm'>
-                  {/* <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            <div className={classes.heroContent}>
+              <Container maxWidth="sm">
+                {/* <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
                     Oops! Nothing here.
                   </Typography> */}
-                </Container>
-              </div>
-            )}
+              </Container>
+            </div>
+          )}
 
           <Fab
-            aria-label='Add'
+            aria-label="Add"
             className={classes.fab}
-            color='primary'
+            color="primary"
             onClick={this.handleClickOpen}
           >
             <AddIcon />
           </Fab>
 
           <Backdrop className={classes.backdrop} open={this.state.backdrop}>
-            <CircularProgress color='inherit' />
+            <CircularProgress color="inherit" />
           </Backdrop>
 
           <UrlsDialog
@@ -464,7 +505,7 @@ class Admin extends Component {
             autoHideDuration={6000}
             onClose={this.handleToastClose}
           >
-            <Alert onClose={this.handleToastClose} severity='success'>
+            <Alert onClose={this.handleToastClose} severity="success">
               Successfully added!
             </Alert>
           </Snackbar>
@@ -477,15 +518,18 @@ class Admin extends Component {
 function mapStateToProps(state) {
   return {
     isLoggingOut: state.auth.isLoggingOut,
-    links: getFilteredLinks(state.links, state.filter)
+    links: getFilteredLinks(state.links, state.filter),
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     addLink: (data) => dispatch(addLink(data)),
-    setLink: (data) => dispatch(setLinks(data))
+    setLink: (data) => dispatch(setLinks(data)),
+    logout: () => dispatch(logoutUser()),
   };
 }
 
-export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Admin));
+export default withStyles(styles)(
+  connect(mapStateToProps, mapDispatchToProps)(Admin)
+);
