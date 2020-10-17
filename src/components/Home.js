45aab10dc2400c90ba4613755082d291b25bad5d
Restyled.io <commits@restyled.io>
2020-10-17 16:20:51 +0000

    Restyled by prettier

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,10 @@ import "firebase/firestore";
 import "../config";
 
 import firebase from "firebase/app";
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import {db} from "../firebase/firebase";
-import {getUserIP} from "../services/ip";
+import { db } from "../firebase/firebase";
+import { getUserIP } from "../services/ip";
 
 import classes from "./components.module.css";
 
@@ -13,104 +13,112 @@ class Home extends Component {
   constructor() {
     super();
     this.state = {
-      loc : window.location.pathname,
-      newloc : "",
-      loading : true,
-      isLocked : true,
-      realPassword : "",
-      password : "",
+      loc: window.location.pathname,
+      newloc: "",
+      loading: true,
+      isLocked: true,
+      realPassword: "",
+      password: "",
     };
     document.body.style =
-        "background: #f1f1f1; margin: 0; padding: 0; min-height: 100vh; display: flex;justify-content: center;align-items: center;";
+      "background: #f1f1f1; margin: 0; padding: 0; min-height: 100vh; display: flex;justify-content: center;align-items: center;";
   }
 
-  componentDidMount() { this.startFunc(true); }
+  componentDidMount() {
+    this.startFunc(true);
+  }
 
   hitCount = async (docid, author) => {
     const hitref = db.collection("hits").doc(docid);
     try {
       await hitref.update({
-        hits : firebase.firestore.FieldValue.increment(1),
+        hits: firebase.firestore.FieldValue.increment(1),
       });
     } catch (err) {
       // hit document is not created yet.
-      await hitref.set({hits : 1, curl : docid, author});
+      await hitref.set({ hits: 1, curl: docid, author });
     }
   };
 
   getGeoReference = async (ip) => {
     try {
-      const response = await fetch('https://freegeoip.app/json/' + ip);
+      const response = await fetch("https://freegeoip.app/json/" + ip);
 
       return response.json();
     } catch (ex) {
-      console.warn('Was not possible to fetch ip info', ex);
+      console.warn("Was not possible to fetch ip info", ex);
       return {};
     }
   };
 
   startFunc = (isLocked) => {
-    this.setState({loading : true});
+    this.setState({ loading: true });
     if (this.state.loc === "/") {
-      this.setState({newloc : global.config.mainsite});
+      this.setState({ newloc: global.config.mainsite });
       window.location = global.config.mainsite;
     } else {
       var docid = this.state.loc.substring(1);
       var docref = db.collection("shorturls").doc(docid);
-      docref.get()
-          .then((doc) => {
-            var data = doc.data();
-            this.setState({realPassword : data.password});
-            if (!doc.exists) {
-              window.location.pathname = "/login";
+      docref
+        .get()
+        .then((doc) => {
+          var data = doc.data();
+          this.setState({ realPassword: data.password });
+          if (!doc.exists) {
+            window.location.pathname = "/login";
+          } else {
+            if (!data.locked || !isLocked) {
+              this.setState({ isLocked: false });
+              // update the hits count
+              this.hitCount(docid, data.author).then(() => {
+                if (data.track === false) {
+                  this.setState({ newloc: data.lurl });
+                  window.location = data.lurl;
+                } else {
+                  getUserIP().then(async (ip) => {
+                    const { country_code } = await this.getGeoReference(ip);
+                    docref
+                      .collection("tracking")
+                      .add({
+                        ipv4: ip,
+                        timestamp: new Date().toLocaleString(),
+                        useragent: navigator.userAgent,
+                        country: country_code,
+                        author: data.author,
+                      })
+                      .then(() => {
+                        this.setState({ newloc: data.lurl });
+                        window.location = data.lurl;
+                      });
+                  });
+                }
+              });
             } else {
-              if (!data.locked || !isLocked) {
-                this.setState({isLocked : false});
-                // update the hits count
-                this.hitCount(docid, data.author).then(() => {
-                  if (data.track === false) {
-                    this.setState({newloc : data.lurl});
-                    window.location = data.lurl;
-                  } else {
-                    getUserIP().then(async (ip) => {
-                      const {country_code} = await this.getGeoReference(ip);
-                      docref.collection("tracking")
-                          .add({
-                            ipv4 : ip,
-                            timestamp : new Date().toLocaleString(),
-                            useragent : navigator.userAgent,
-                            country : country_code,
-                            author : data.author,
-                          })
-                          .then(() => {
-                            this.setState({newloc : data.lurl});
-                            window.location = data.lurl;
-                          });
-                    });
-                  }
-                });
-              } else {
-                this.setState({loading : false, isLocked : true});
-              }
+              this.setState({ loading: false, isLocked: true });
             }
-          })
-          .catch((err) => { console.log("Error getting document", err); });
+          }
+        })
+        .catch((err) => {
+          console.log("Error getting document", err);
+        });
     }
   };
 
   checkpassword = () => {
-    const {realPassword} = this.state;
+    const { realPassword } = this.state;
     if (realPassword !== "" && this.state.password === realPassword) {
-      this.setState({isLocked : false}, () => this.startFunc(false));
+      this.setState({ isLocked: false }, () => this.startFunc(false));
     } else {
       alert("Invalid Link Password");
     }
   };
 
-  onPasswordChange = (e) => { this.setState({password : e.target.value}); };
+  onPasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  };
 
   render() {
-    const {loading, password, isLocked} = this.state;
+    const { loading, password, isLocked } = this.state;
     return (
       <div>
         {loading ? (
@@ -143,9 +151,15 @@ class Home extends Component {
               <button
                 onClick={(e) => this.checkpassword()}
                 style={{
-      padding: "12px", color: "white", backgroundColor: "black",
-          fontSize: "15px", border: "none", marginRight: "15px",
-          borderRadius: "5px", cursor: "pointer", marginLeft: "10px",
+                  padding: "12px",
+                  color: "white",
+                  backgroundColor: "black",
+                  fontSize: "15px",
+                  border: "none",
+                  marginRight: "15px",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                  marginLeft: "10px",
                 }}
               >
                 Go
