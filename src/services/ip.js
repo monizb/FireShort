c32cb916cd8725f3ff38cf4ca74c14c331d4d2c0
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-10-17 21:42:35 +0530

    :rocket: other: using cloudfare service to get user IP

---
+++ src/services/ip.js
@@ -0,0 +1,9 @@
+export const getUserIP = async () => {
+  const CLOUDFARE_API_URL = "https://www.cloudflare.com/cdn-cgi/trace";
+  const response = await fetch(CLOUDFARE_API_URL);
+  const data = await response.text();
+  if (data.includes("ip")) {
+    const lines = data.split("\n");
+    return lines[2].split("=")[1];
+  }    
+};
