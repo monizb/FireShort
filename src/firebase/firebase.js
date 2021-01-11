667594742bc46de4976cc33e9457d0f2410742c5
Amit kumar mishra <akm43300@gmail.com>
2021-01-11 22:19:27 +0530

    updated #165

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,15 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCYtW0exSgik_v3ESrawfV6A8QfG6e94JQ",
-  authDomain: "new-project-1143a.firebaseapp.com",
-  projectId: "new-project-1143a",
-  storageBucket: "new-project-1143a.appspot.com",
-  messagingSenderId: "1059947234987",
-  appId: "1:1059947234987:web:1c0a86106c460a10b559ee",
-  measurementId: "G-C4RD75MEZQ"
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEAS_ID
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
