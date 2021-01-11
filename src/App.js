7ec541e6f53333f04991ca0b85f88e8fee563034
Restyled.io <commits@restyled.io>
2021-01-11 16:52:36 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { connect } from "react-redux";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Login from "./components/Login";
-import Home from "./components/Home";
+import {connect} from "react-redux";
+import {Route, Switch} from "react-router-dom";
+
 import Admin from "./components/Admin";
 import Analytics from "./components/Analytics";
-import SignUp from "./components/Signup";
 import ForgotPassword from "./components/ForgotPassword";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import SignUp from "./components/Signup";
 
 function App(props) {
-  const { isAuthenticated, isVerifying } = props;
+  const {isAuthenticated, isVerifying} = props;
   return (
     <Switch>
       <ProtectedRoute
-        exact
-        path="/analytics/:id"
-        component={Analytics}
-        isAuthenticated={isAuthenticated}
-        isVerifying={isVerifying}
-      />
+  exact
+  path = "/analytics/:id"
+  component = {Analytics} isAuthenticated = {isAuthenticated} isVerifying =
+  { isVerifying } />
       <ProtectedRoute
         exact
-        path="/admin"
+        path="/admin "
         component={Admin}
         isAuthenticated={isAuthenticated}
-        isVerifying={isVerifying}
+        isVerifying={
+    isVerifying}
       />
       <Route path="/login" component={Login} />
-      <Route path="/signup" component={SignUp} />
+      <Route path="/signup" component={
+    SignUp} />
       <Route path="/forgot-password" component={ForgotPassword} />
-      <Route path="/" component={Home} />
+      <Route path="/" component={
+    Home} />
     </Switch>
   );
 }
 
 function mapStateToProps(state) {
   return {
-    isAuthenticated: state.auth.isAuthenticated,
-    isVerifying: state.auth.isVerifying
+    isAuthenticated : state.auth.isAuthenticated,
+    isVerifying : state.auth.isVerifying
   };
 }
 
