aeec60a50084c97eb4638b44abaffe91be1db42d
Restyled.io <commits@restyled.io>
2021-01-13 08:08:25 +0000

    Restyled by prettier

--- src/components/LoginLeftComponent.js
+++ src/components/LoginLeftComponent.js
@@ -1,53 +1,51 @@
 import "./logleft.css";
 
-import {Grid} from "@material-ui/core";
-import {withStyles} from "@material-ui/styles";
+import { Grid } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
 import React from "react";
 
 import FeatureCard from "./FeatureCard";
 
 const styles = () => ({
-  loginLeftPart : {
-    width : "60%",
-    height : "initial",
-    marginTop : "50px",
-    padding : "50px 50px",
-    display : "flex",
-    flexDirection : "column",
-    justifyContent : "center",
-    alignItems : "center",
-    overflow : "hidden",
-    animation : "$colorEffect 10s ease-in-out",
-    animationIterationCount : "infinite",
+  loginLeftPart: {
+    width: "60%",
+    height: "initial",
+    marginTop: "50px",
+    padding: "50px 50px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+    animation: "$colorEffect 10s ease-in-out",
+    animationIterationCount: "infinite",
   },
-  loginImage : {
-    width : "30rem",
-    transition : "all 0.1s ease-in",
-    "&:hover" : {transform : "scale(1.05)"},
-
+  loginImage: {
+    width: "30rem",
+    transition: "all 0.1s ease-in",
+    "&:hover": { transform: "scale(1.05)" },
   },
-  featureContainer : {
-    width : "50%",
-    alignItems : "center",
-    colorEffect : "grey",
-    marginRight : "300px",
+  featureContainer: {
+    width: "50%",
+    alignItems: "center",
+    colorEffect: "grey",
+    marginRight: "300px",
   },
-  "@media (max-width: 820px)" : {
-    loginImage : {
-      transform : "translateY(300px)",
-      width : "200px",
+  "@media (max-width: 820px)": {
+    loginImage: {
+      transform: "translateY(300px)",
+      width: "200px",
     },
-    loginLeftPart : {
-      transform : "translateY(300px)",
-      width : "200px",
-    }
-  }
+    loginLeftPart: {
+      transform: "translateY(300px)",
+      width: "200px",
+    },
+  },
 });
 
-const LoginLeftComponent = ({classes}) => {
+const LoginLeftComponent = ({ classes }) => {
   return (
     <Grid item className={classes.loginLeftPart}>
-      
       <div className={classes.featureContainer}>
         <FeatureCard feature="Custom Tiny URLs" />
         <FeatureCard feature="Hit Tracker" />
